03ebf9f6285af83fdc773a94dc1bdef25c5f4839
Jaylen Guerrero <jaylenthe1st@gmail.com>
2025-03-08 11:08:42 -0700

    added experiment

--- HashtableExperiment.java
+++ HashtableExperiment.java
@@ -1,5 +1,12 @@
 public class HashtableExperiment {
     public static void main(String[] args) {
-        
+        Hashtable linearHash = new LinearProbing(10);
+        HashObject object1 = new HashObject("Hello");
+
+        if (linearHash.search(object1) == object1) {
+            System.out.println("Object found");
+        } else {
+            System.out.println("Object not found");
+        }
     }
 }
